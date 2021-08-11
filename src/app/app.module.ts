f06996fdcdb6a3fe2f5672083614a4fe7cafda99
MateusFirmino <mateusfirmino1@hotmail.com>
2021-08-11 14:37:08 -0400

    Implementações gráficas, pagina inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { FooterComponent } from './components/template/footer/footer.component';
+import {RouterModule} from "@angular/router";
+import {ComponentsModule} from "./components/components.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
