a201f833bb6a5254e622f7ae0962a5788224342d
Mateus Firmino <mateusfirmino1@hotmail.com>
2021-08-18 18:03:45 -0400

    UPDATE para Livro implementado

--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {Livro} from "./livro.model";
 import {HttpClient} from "@angular/common/http";
@@ -20,11 +20,22 @@ export class LivroService {
     return this.http.get<Livro[]>(url)
   }
 
-  create(livro: Livro, id_cat: String): Observable<Livro>{
+  findById(id: String):Observable<Livro>{
+    const url = `${this.baseUrl}/livros/${id}`
+    return this.http.get<Livro>(url)
+  }
+
+  update(livro: Livro):Observable<Livro> {
+    const url = `${this.baseUrl}/livros/${livro.id}`
+    return this.http.put<Livro>(url, livro)
+  }
+
+  create(livro: Livro, id_cat: String): Observable<Livro> {
     const url = `${this.baseUrl}/livros?categoria=${id_cat}`
-    return this.http.post<Livro>(url,livro)
+    return this.http.post<Livro>(url, livro)
 
   }
+
   mensagem(str: String): void {
     this._snack.open(`${str}`, 'OK', {
       horizontalPosition: 'end',
