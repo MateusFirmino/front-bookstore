5835220d0ed9f7caf16b5ab005c2583348481ce5
MateusFirmino <mateusfirmino1@hotmail.com>
2021-08-18 08:17:10 -0400

    Categoria DELETE implementada

--- src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
+++ src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoriaService} from "../categoria.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Categoria} from "../categoria.model";
 
 @Component({
   selector: 'app-categoria-delete',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria-delete.component.css']
 })
 export class CategoriaDeleteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: ''
   }
 
+  constructor(private service: CategoriaService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!
+    this.findById()
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe((resposta) =>{
+      this.categoria.nome = resposta.nome
+      this.categoria.descricao = resposta.descricao
+      console.log(this.categoria)
+    })
+  }
+  delete(): void {
+    this.service.delete(this.categoria.id!).subscribe((reposta) =>{
+      this.router.navigate(['categorias'])
+      this.service.mensagem('Categoria Deletada com Sucesso!')
+    },  err => {
+      this.service.mensagem(err.error.error)
+    })
+  }
+  cancel():void {
+    this.router.navigate(['categorias'])
+  }
 }
